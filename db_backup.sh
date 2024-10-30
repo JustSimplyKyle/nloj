9c2f2d3ab86ab0e3124f7e47c7b563be24ad5c4a
justsimplykyle <shiue.kyle@gmail.com>
2024-10-30 19:01:56 +0000

    feat: database-update

---
+++ db_backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set path to your project directory
+BACKUP_DIR="/home/cdx-user/tioj"
+BACKUP_FILE="backup.sql"
+
+# Change to project directory
+cd "$BACKUP_DIR" || exit 1
+
+# Perform database backup
+if mysqldump -u root -p'nlhs' tioj_production > "$BACKUP_FILE"; then
+    # Git operations
+    git add "$BACKUP_FILE"
+    git add td
+    git add public
+	git add db_backup.sh
+    git commit -m "feat: database-update"
+    git push
+fi
