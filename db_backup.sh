bb8a8d3e87f6e533ca083246d979c5ba5a74c8ad
justsimplykyle <shiue.kyle@gmail.com>
2024-11-24 09:09:05 +0000

    feat: database-update

--- db_backup.sh
+++ db_backup.sh
@@ -9,12 +9,12 @@ cd "$BACKUP_DIR" || exit 1
 
 # Perform database backup
 if mysqldump -u root -p'nlhs' tioj_production > "$BACKUP_FILE"; then
+    git pull
     # Git operations
     git add "$BACKUP_FILE"
     git add td
     git add public
 	git add db_backup.sh
     git commit -m "feat: database-update"
-    git pull
     git push
 fi
